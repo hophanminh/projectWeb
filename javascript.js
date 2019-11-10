b76d1e5f5f792aa5e89d12cb4c3d04e18c346419
Hồ Phan Minh <38271639+Akashi195@users.noreply.github.com>
2019-11-10 22:47:49 +0700

    Menu
    Delete Multi level menu
    Use normal Menu ( dropdown)

--- javascript.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.dropdown-submenu div#sub').on("click", function(e){
-      $(this).next('#sub').toggle();
-      e.stopPropagation();
-      e.preventDefault();
-    });
-  });
